75360c53ccff878303fa70c060d81ea35e94d2f9
JosephYusufov <jyusufov00@stuy.edu>
2019-09-23 18:34:47 -0400

    final ver of 10_occ

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -18,12 +18,12 @@ with open('./static/occupations.csv') as csv_file:  # open CSV file
 
 def selector():  # function to randomly select an occupation
     randomValue = random.random() * 99.8  # Pick a random number from 0 to 99.8
-    threshold = 0
-    for x, y in OCCUPATIONS.items():
-        if(randomValue < threshold):
-            return(x, y)
+    threshold = 0  # establish a counter
+    for x, y in OCCUPATIONS.items():  # for every entry in the dictionary (key, value)
+        if(randomValue < threshold):  # if the random number is less than the threshold
+            return(x, y)  # return that entry
         else:
-            threshold += y
+            threshold += y  # otherwise add this value to the threshold
 
 @app.route("/")
 def hello_world():
@@ -35,10 +35,12 @@ def hello_world():
 def index():
     return render_template("index.html")
 
-@app.route("/occupyflaskst")
+@app.route("/occupyflaskst")  # route traffic to http://localhost:5000/occupyflaskst
 def test_tmplt():
-    selected = selector()
-    return render_template("index.html", OCCUPATIONS=OCCUPATIONS, selected=selected)
+    selected = selector()  # select a weighted random occupation
+
+    # Render the template with the entire ictionary to be printed, and the chosen value
+    return render_template("index.html", OCCUPATIONS=OCCUPATIONS, selected=selected)  
 
 if __name__ == "__main__":
     app.debug = True
