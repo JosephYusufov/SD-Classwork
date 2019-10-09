cf6c5a96fc598b9d6f2c0fe7241f71198b89405c
JosephYusufov <jyusufov00@stuy.edu>
2019-10-09 10:42:25 -0400

    ommitted intermediate dictionaries, able to add data from csv into database tables

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -1,5 +1,5 @@
 #Joseph Yusufov, With a skeleton provided by Mr. Mykolyk
-#SoftDev  
+#SoftDev
 #skeleton :: SQLITE3 BASICS
 #Oct 10 2019
 
@@ -9,40 +9,42 @@ import csv       #facilitate CSV I/O
 STUDENTS = {} # Dictionary to hold a dictionary for each student
 COURSES = {}  # Dictionary to hold a dictionary for each course
 
-with open('students.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        THIS = {'name': row['name'], 'age': row['age']} # Dict for each student
-        STUDENTS[row['id']] = THIS # Populate Super Dict with Sub Dicts
-
-with open('courses.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        # Dict for each course
-        THIS = {'code': row['code'], 'mark': row['mark'], 'id': row['id']}
-        COURSES[row['id']] = THIS  # Populate Super Dict with Sub Dicts
-
-
-
 DB_FILE="discobandit.db"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
+
+insert_data_student = ""
+with open('students.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        insert_data_student += "INSERT INTO student VALUES('" + row['name'] + "'," + row['age'] + ", " + row['id'] + ");\n"
+print(insert_data_student)
+
+
+insert_data_courses = ""
+with open('courses.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        insert_data_courses += "INSERT INTO courses VALUES('" + row['code'] + "'," + row['mark'] + ", " + row['id'] + ");\n"
+print(insert_data_courses)
+
+
 #==========================================================
 
 # < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
-insert_data_student = "" # generate the SQL script to be ran, reading from the STUDENTS dictionary as populated above
-for student in STUDENTS:
-    insert_data_student += "INSERT INTO student VALUES('" + STUDENTS[student]['name'] + "'," + STUDENTS[student]['age'] + ", " + student + ");\n"   
-print(insert_data_student)
-
-print('\n \n')
-# generate the SQL script to be ran, reading from the STUDENTS dictionary as populated above
-insert_data_courses = ""
-for course in COURSES:
-    insert_data_courses += "INSERT INTO courses VALUES('" + COURSES[course]['code'] + "'," + COURSES[course]['mark'] + ", " + course + ");\n"
-print(insert_data_courses)
+# insert_data_student = "" # generate the SQL script to be ran, reading from the STUDENTS dictionary as populated above
+# for student in STUDENTS:
+#     insert_data_student += "INSERT INTO student VALUES('" + STUDENTS[student]['name'] + "'," + STUDENTS[student]['age'] + ", " + student + ");\n"
+# print(insert_data_student)
+#
+# print('\n \n')
+# # generate the SQL script to be ran, reading from the STUDENTS dictionary as populated above
+# insert_data_courses = ""
+# for course in COURSES:
+#     insert_data_courses += "INSERT INTO courses VALUES('" + COURSES[course]['code'] + "'," + COURSES[course]['mark'] + ", " + course + ");\n"
+# print(insert_data_courses)
 
 
 # test SQL stmt in sqlite3 shell, save as string
@@ -58,5 +60,3 @@ c.executescript(insert_data_courses)    # run SQL statement
 
 db.commit() #save changes
 db.close()  #close database
-
-
