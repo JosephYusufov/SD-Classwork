bb5fd62762f7eb11781a8921d2b47bfc3643b99c
JosephYusufov <jyusufov00@stuy.edu>
2019-10-14 02:15:00 -0400

    Too difficult... Going to sleep

--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -11,17 +11,21 @@ DB_FILE = "discobandit.db"
 db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
 c = db.cursor()  # facilitate db ops
 print_student = "SELECT * FROM student;"
-select_courses_frag = "SELECT mark FROM courses WHERE id == " 
+select_courses_frag = "SELECT mark FROM courses, student WHERE courses.id = %s;" 
 # print_courses = "SELECT * FROM courses;"
 
-student_toprint = c.execute(print_student)
-for member in student_toprint:
+c.execute(print_student)
+row = c.fetchone()
+while row is not None:
     print("---")
-    # print(member[2])
-    print(c.execute(select_courses_frag + str(member[2]) + ';' )) # Needs a nested for loop, and more revision. 
+    id_str = row[2]
+    courses = c.execute(select_courses_frag % id_str)
+    print(courses)
     print("---")
+    row = c.fetchone()
 
-    
+
+# print(course_selection_script)
 
 # courses_toprint = c.execute(print_courses)
 # for member in courses_toprint:
