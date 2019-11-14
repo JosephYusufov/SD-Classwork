c69521cf303254bf8164d1625b1df58bd5bacd23
JosephYusufov <jyusufov00@stuy.edu>
2019-11-14 00:10:05 -0500

    citibike and rick and morty completed

--- 25_restrio/app.py
+++ 25_restrio/app.py
@@ -6,6 +6,7 @@
 from flask import Flask, render_template
 import urllib
 import json
+import random
 
 app = Flask(__name__)  # create instance of class Flask
 
@@ -15,15 +16,29 @@ def hello_world():
     return render_template("index.html")
 
 
-@app.route("/rickandmorty")  
+@app.route("/rickandmorty")
 def rickandmorty():
     r = urllib.request.urlopen(
-                     "https://rickandmortyapi.com/api/character/"
-                     )
+        "https://rickandmortyapi.com/api/character/"
+    )
     data = json.loads(r.read())
     # print(data);
-    print(data['results'][0])
+    print(data['results'][0]['name'])
     return render_template("rickandmorty.html", data=data)
+
+
+@app.route("/citi")
+def citi():
+    r = urllib.request.urlopen(
+        "http://api.citybik.es/v2/networks"
+    )
+    response = json.loads(r.read())
+    data = response['networks']
+    # print(data)
+    # print(data['results'][0]['name'])
+    station = random.choice(list(data))
+    # print(station)
+    return render_template("citi.html", data=station)
 
 if __name__ == "__main__":
     app.debug = True
