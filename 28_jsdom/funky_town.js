60869d3c13a031a3c71469397a834d741bf4da88
JosephYusufov <jyusufov00@stuy.edu>
2019-12-12 09:04:37 -0500

    color changes

--- 28_jsdom/funky_town.js
+++ 28_jsdom/funky_town.js
@@ -55,6 +55,8 @@ var factButton = document.getElementById("factButton").addEventListener("click",
       var number = parseInt(document.getElementById("factInput").value);
       console.log(fact(number));
       document.getElementById("factAnswer").innerHTML = fact(number);
+      document.getElementById("factAnswer").style.color = "green";
+      document.getElementById("factA").style.color = "green";
       return fact(number);
     }
 );
@@ -64,6 +66,8 @@ var fibButton = document.getElementById("fibButton").addEventListener("click",
       var number = parseInt(document.getElementById("fibInput").value);
       console.log(fibonacci(number));
       document.getElementById("fibAnswer").innerHTML = fibonacci(number);
+      document.getElementById("fibAnswer").style.color = "green";
+      document.getElementById("fibA").style.color = "green";
       return fibonacci(number);
     }
 );
@@ -73,6 +77,8 @@ var studentButton = document.getElementById("studentButton").addEventListener("c
       var student = randomStudent();
       console.log(student);
       document.getElementById("studentAnswer").innerHTML = student;
+      document.getElementById("studentAnswer").style.color = "green";
+      document.getElementById("studentA").style.color = "green";
       return student;
     }
 );
@@ -83,6 +89,8 @@ var gcdButton = document.getElementById("gcdButton").addEventListener("click",
       var number1 = parseInt(document.getElementById("gcdInput1").value);
       console.log(gcd(number0, number1));
       document.getElementById("gcdAnswer").innerHTML = gcd(number0, number1);
+      document.getElementById("gcdAnswer").style.color = "green";
+      document.getElementById("gcdA").style.color = "green";
       return gcd(number0, number1);
     }
 );
