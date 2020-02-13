79e3eea0e385582339e2b66d7a6698b8f5e1de2e
JosephYusufov <jyusufov00@stuy.edu>
2020-02-13 09:31:53 -0500

    classwork on 08 with Hilary Zen

--- 08_canvas-anim/animate.js
+++ 08_canvas-anim/animate.js
@@ -4,6 +4,7 @@ var radius = 20;
 var increasing = 0; //0 if increasing, 1 if decreasing
 var state = 0; //0 if stopped, 1 if running
 var id;
+var dvdImage = document.getElementById("dvd-image");
 
 var animateFunction = function() {
   //console.log("animate called");
@@ -39,8 +40,40 @@ var stopFunction = function() {
   window.cancelAnimationFrame(id);
 }
 
+var dvdX = 100;
+var dvdY = 100;
+
+var dvdAnimate = function() {
+    // if (!state) {
+    // 	state = 1;
+    randomizeDVD();
+    dvdFunction();
+    // }
+}
+var dvdFunction = function() {
+    id = window.requestAnimationFrame(dvdFunction)
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.drawImage(dvdImage,dvdX,dvdY,100,70);
+    console.log("(" + dvdX + ", " + dvdY + ")");
+    dvdX += 1;
+    dvdY += 1;
+}
+
+
 var animate = document.getElementById("animate");
 animate.addEventListener("click", animateFunction);
 
 var stop = document.getElementById("stop");
 stop.addEventListener("click", stopFunction);
+
+var dvd = document.getElementById("dvd-button");
+dvd.addEventListener("click", dvdAnimate);
+
+// Helper Function
+var randomizeDVD = function() {
+    dvdX = Math.floor(Math.random() * 400);
+    dvdY = Math.floor(Math.random() * 400);
+}
+
+// DVD speeds up
+// Doesnt bounce
