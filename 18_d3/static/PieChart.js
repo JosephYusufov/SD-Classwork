e45d57e40bf24697677964bc78878aec1999e12e
Joseph Yusufov <jyusufov00@stuy.edu>
2020-04-21 15:06:27 -0400

    wrote a bunch of helper functions to make the data more accessible

--- 18_d3/static/PieChart.js
+++ 18_d3/static/PieChart.js
@@ -3,46 +3,62 @@ fetch("https://data.cityofnewyork.us/api/views/ihfw-zy9j/rows.json?accessType=DO
         return response.json();
     }).then((data) => {
         process(data);
-    })
+    });
+var width = 500;
+var height = 500;
 
 const process = (data) => {
-    pie = d3.pie()
-        .sort(null)
-        .value(d => d.value)
+    // const p = d3.selectAll("p");
+    // console.log(p);
+    // console.log(data);
 
-    console.log(data);
-    const arcs = pie(data);
+    // HELPER FUNCTIONS
+    const getByYear = (schoolYear) => data.data.filter(datum => datum[10] == schoolYear)
 
-    const svg = d3.create("svg")
-        .attr("viewBox", [-width / 2, -height / 2, width, height]);
+    const getPercentageByRace = (race, data) => {
+        const raceIndex = (raceParam => ({
+                'a': 35,
+                'b': 37,
+                'h': 39,
+                'w': 41
+        }[raceParam]))(race);
+        return d3.mean(data.map((d => d[raceIndex])));
+    }
 
-    svg.append("g")
-        .attr("stroke", "white")
-        .selectAll("path")
-        .data(arcs)
-        .join("path")
-        .attr("fill", d => color(d.data.name))
-        .attr("d", arc)
-        .append("title")
-        .text(d => `${d.data.name}: ${d.data.value.toLocaleString()}`);
+    const getRaceSummary = (data) => {
+        const summary = {    
+            asian: getPercentageByRace('a', data),
+            black: getPercentageByRace('b', data),
+            hispanic: getPercentageByRace('h', data),
+            white: getPercentageByRace('w', data),
+        } 
+        summary['sum'] = d3.sum(Object.values(summary));
+        return summary;
+    };
 
-    svg.append("g")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", 12)
-        .attr("text-anchor", "middle")
-        .selectAll("text")
-        .data(arcs)
-        .join("text")
-        .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
-        .call(text => text.append("tspan")
-            .attr("y", "-0.4em")
-            .attr("font-weight", "bold")
-            .text(d => d.data.name))
-        .call(text => text.filter(d => (d.endAngle - d.startAngle) > 0.25).append("tspan")
-            .attr("x", 0)
-            .attr("y", "0.7em")
-            .attr("fill-opacity", 0.7)
-            .text(d => d.data.value.toLocaleString()));
+    console.log(getRaceSummary(getByYear('20102011')));
+    // console.log(getPercentageByRace('w', getByYear('20052006')))
+    // console.log();
+//     const pie = d3.pie()
+//         .sort(null)
+//         .value(d => d.value);
 
-    return svg.node();
+//     const arcLabel = () => {
+//         const radius = math.min(width, height) / 2 * 0.8;
+//         return d3.arc().innerRadius(radius).outerRadius(radius);
+//     };
+
+//     const arc = d3.arc()
+//         .innerRadius(0)
+//         .outerRadius(Math.min(width, height) / 2 - 1);
+
+//     const color = d3.scaleOrdinal()
+//         .domain(data.map(d => d.name))
+//         .range(d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), data.length).reverse());
 };
+
+
+
+// d3.json('https://data.cityofnewyork.us/api/views/ihfw-zy9j/rows.json?accessType=DOWNLOAD', 
+//     data => process(data));
+
